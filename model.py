f7149a58345957ce219505c6104e0667a15d64b2
spikal2000 <73543919+spikal2000@users.noreply.github.com>
2023-05-06 15:28:03 +0300

    first working model

--- model.py
+++ model.py
@@ -8,6 +8,7 @@ Created on Sat May  6 11:30:57 2023
 import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.ensemble import ExtraTreesRegressor
+from sklearn.metrics import r2_score
 
 data = pd.read_csv("new_crop_data.csv")
 crop_data = pd.read_csv("Crop_Yield_Data_challenge_2.csv")
@@ -18,7 +19,7 @@ y = crop_data['Rice Yield (kg/ha)']
 X_train, X_test, y_train, y_test =  train_test_split(X, y, test_size=0.2, random_state=21)
 
 #Model Training
-regressor = ExtraTreesRegressor(bootstrap=False, ccp_alpha=0.0, criterion='mse',
+regressor = ExtraTreesRegressor(bootstrap=False, ccp_alpha=0.0, criterion='squared_error',
                     max_depth=None, max_features='auto', max_leaf_nodes=None,
                     max_samples=None, min_impurity_decrease=0.0, min_samples_leaf=1,
                     min_samples_split=2, min_weight_fraction_leaf=0.0,
@@ -27,3 +28,10 @@ regressor = ExtraTreesRegressor(bootstrap=False, ccp_alpha=0.0, criterion='mse',
 regressor.fit(X_train, y_train)
 
 #model Evaluation
+#In sample evaluation
+insample_predictions = regressor.predict(X_train)
+print("Insample R2 Score: {0:.2f}".format(r2_score(y_train,insample_predictions)))
+
+#Out-sample evaluation
+outsample_predictions = regressor.predict(X_test)
+print("Outsample R2 Score: {0:.2f}".format(r2_score(y_test,outsample_predictions)))
