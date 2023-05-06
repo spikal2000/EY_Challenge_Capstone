63acd918c14da69e6c2d1607c2940daf88f85a07
spikal2000 <73543919+spikal2000@users.noreply.github.com>
2023-05-06 17:02:34 +0300

    ExtraTreesRegressor working
    TODO: propably add more data

--- model.py
+++ model.py
@@ -9,6 +9,7 @@ import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.ensemble import ExtraTreesRegressor
 from sklearn.metrics import r2_score
+from sklearn.preprocessing import StandardScaler
 
 data = pd.read_csv("new_crop_data.csv")
 crop_data = pd.read_csv("Crop_Yield_Data_challenge_2.csv")
@@ -18,20 +19,38 @@ y = crop_data['Rice Yield (kg/ha)']
 
 X_train, X_test, y_train, y_test =  train_test_split(X, y, test_size=0.2, random_state=21)
 
-#Model Training
-regressor = ExtraTreesRegressor(bootstrap=False, ccp_alpha=0.0, criterion='squared_error',
-                    max_depth=None, max_features='auto', max_leaf_nodes=None,
-                    max_samples=None, min_impurity_decrease=0.0, min_samples_leaf=1,
-                    min_samples_split=2, min_weight_fraction_leaf=0.0,
-                    n_estimators=100, n_jobs=-1, oob_score=False,
-                    random_state=123, verbose=0, warm_start=False)
-regressor.fit(X_train, y_train)
+def ExtraTreesR(X_train, y_train, n_estimators=100, max_depth=None, min_samples_split=2,max_features='auto',
+                bootstrap=False):
+    regressor = ExtraTreesRegressor(n_estimators=n_estimators, 
+                                    max_depth=max_depth,
+                                    min_samples_split=min_samples_split,
+                                    max_features=max_features,
+                                    bootstrap=bootstrap
+                                    )
+    regressor.fit(X_train, y_train)
+    #model Evaluation
+    print("============ExtraTreesRegressor============")
+    #In sample evaluation
+    insample_predictions = regressor.predict(X_train)
+    print("Insample R2 Score: {0:.2f}".format(r2_score(y_train,insample_predictions)))
 
-#model Evaluation
-#In sample evaluation
-insample_predictions = regressor.predict(X_train)
-print("Insample R2 Score: {0:.2f}".format(r2_score(y_train,insample_predictions)))
+    #Out-sample evaluation
+    outsample_predictions = regressor.predict(X_test)
+    print("Outsample R2 Score: {0:.2f}".format(r2_score(y_test,outsample_predictions)))
+    return regressor
 
-#Out-sample evaluation
-outsample_predictions = regressor.predict(X_test)
-print("Outsample R2 Score: {0:.2f}".format(r2_score(y_test,outsample_predictions)))
+"""
+# Use the ExtraTreesRegressor
+model = ExtraTreesR(X_train, y_train, 
+                    n_estimators=110, 
+                    max_depth=5, 
+                    min_samples_split=10,max_features='auto',
+               bootstrap=True)
+
+new_data = X_test[:1]
+y_new = y_test[:1]
+predctions = model.predict(new_data)
+
+print("Actual:", y_new, "Predicted:", predctions)
+"""
+
